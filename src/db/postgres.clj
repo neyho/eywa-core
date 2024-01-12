8e6fe431bb7090f906404b3aa3d4cd04a1a91d01
Robert Geršak <robi@neyho.com>
2024-01-12 14:48:38 +0100

    Adding logback for db alias and new updated frontend

--- src/db/postgres.clj
+++ src/db/postgres.clj
@@ -1,7 +1,9 @@
 (ns postgres
   (:require
+    [clojure.string :as str]
     [neyho.eywa.data :refer [*ROOT*]]
     [neyho.eywa.administration :as iam]
+    [neyho.eywa.administration.uuids :as au]
     neyho.eywa.transit
     neyho.eywa
     neyho.eywa.lacinia
@@ -10,7 +12,7 @@
     neyho.eywa.db.postgres
     neyho.eywa.avatars.postgres
     neyho.eywa.authorization
-    neyho.eywa.dataset
+    [neyho.eywa.dataset :as dataset]
     neyho.eywa.dataset.core
     neyho.eywa.dataset.default-model
     neyho.eywa.dataset.postgres
@@ -22,13 +24,35 @@
   (neyho.eywa.transit/init)
   (let [db (neyho.eywa.db.postgres/from-env)]
     (neyho.eywa.dataset.core/setup db))
-  (iam/setup
-    {:users
-     [{:name (str admin) :password (str password) :active true
-       :roles [*ROOT*]}]
-     :roles [*ROOT*]}))
+  (when (and admin password)
+    (iam/setup
+      {:users
+       [{:name (str admin) :password (str password) :active true
+         :type :PERSON
+         :roles [*ROOT*]}]
+       :roles [*ROOT*]})))
 
 (defn tear-down
   [_]
   (let [db (neyho.eywa.db.postgres/from-env)]
     (neyho.eywa.dataset.core/tear-down db)))
+
+
+(defn init-eywa
+  []
+  (neyho.eywa.transit/init)
+  (neyho.eywa.db.postgres/init)
+  (neyho.eywa.dataset/init))
+
+
+(defn add-user
+  [{:syms [user password type active]
+    :or {type "PERSON"
+         active true}}]
+  (init-eywa)
+  (dataset/sync-entity
+    au/user
+    {:name (str user)
+     :password (str password)
+     :type (str/upper-case (name type))
+     :active active}))
