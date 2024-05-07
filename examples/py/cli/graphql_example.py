e5c4582733c3f709449f387cbd1293922b47db2c
Robert Geršak <robi@neyho.com>
2024-05-07 09:08:24 +0200

    Added one more query and mutation to graphql_example.py

--- examples/py/cli/graphql_example.py
+++ examples/py/cli/graphql_example.py
@@ -1,4 +1,5 @@
 import eywa
+from datetime import datetime
 
 
 query = """
@@ -12,6 +13,47 @@ query = """
 
 
 response = eywa.graphql({'query': query})
+print('Response:\n' + str(response))
 
 
+query = """
+mutation ($group:UserGroupInput!) {
+  syncUserGroup (user_group: $group) {
+    euuid
+    active
+    modified_on
+    modified_by {
+      name
+    }
+  }
+}
+"""
+
+
+response = eywa.graphql({'query': query, 'variables': {
+    "group": {
+        "name": "Test Grup",
+        "active": True
+        # "modified_on": datetime(2000, 2, 3, 4, 5, 6).isoformat()
+    }
+    }}, 2)
+
+
+print('Response:\n' + str(response))
+
+
+query = """
+{
+  searchUserGroup {
+    euuid
+    active
+    modified_on
+    modified_by {
+      name
+    }
+  }
+}
+"""
+
+response = eywa.graphql({'query': query})
 print('Response:\n' + str(response))
